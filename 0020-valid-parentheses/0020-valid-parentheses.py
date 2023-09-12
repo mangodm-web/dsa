b3b6b47564c24c3815073587c0d8fe8cce00632f
Dami Huh <123475341+mangodm-web@users.noreply.github.com>
2023-09-12 17:12:33 +0900

    Time: 33 ms (90.67%), Space: 16.3 MB (82.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        open_parenthesis = {"(": ")", "[": "]", "{": "}"}
-        close_parenthesis = {")": "(", "]": "[", "}": "{"}
+        pairs = {"(": ")", "[": "]", "{": "}"}
         stack = []
         
-        for letter in s:
-            if (letter in open_parenthesis):
-                stack.append(letter)
-            if (letter in close_parenthesis):
-                if (len(stack) == 0 or stack.pop() != close_parenthesis[letter]):
-                    return False
-        if (stack):
-            return False
-        return True
-    
+        for parenthesis in s:
+            if (parenthesis in pairs):
+                stack.append(parenthesis)
+            elif (len(stack) == 0 or parenthesis != pairs[stack.pop()]):
+                return False
+
+        return len(stack) == 0
