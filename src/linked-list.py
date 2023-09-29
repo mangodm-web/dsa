501fc3bdea79b47959f66e931e5b6348807f6193
mangodm-web <mangodm.web3@gmail.com>
2023-09-29 21:34:39 +0900

    added implementation of LinkedList (1/3)

---
+++ src/linked-list.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.head is None:
+            return None
+
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.head is None:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    # TODO: pop_first, get, set, insert, remove, reverse
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(3)
+my_linked_list.prepend(2)
+
+my_linked_list.print()
